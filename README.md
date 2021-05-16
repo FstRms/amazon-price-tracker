2adcfe886ec892729c2107fc641fa6549509e991
Fausto Ramos <fausto.ramosr92@gmail.com>
2021-05-15 20:05:48 -0500

    Amazon Price Tracker

---
+++ README.md
@@ -0,0 +1,29 @@
+# Amazon Price Tracker
+
+Python Script to track price of a product on Amazon México website and notify via email.
+
+
+### Prerequisites
+
+- Python 3.9+
+- [Poetry 0.12+](https://python-poetry.org/docs/#installation)
+- A Gmail account
+- Email to recieve the notification.
+### Note
+- It is recommended to run this script on [pyhtonanywhere](https://www.pythonanywhere.com/).
+- Or if you are an advanced user you could run it as a cron job on your computer
+#### Installing
+- It is recommended to create a new [gmail account](https://accounts.google.com/signup).
+- Make sure 2-Step Verification is off.
+- Turn on ['Allow less secure apps'](https://myaccount.google.com/lesssecureapps). 
+- Follow the variables in the .env.sample to a .env indicating your own email credentials.
+- Open a terminal and navigate to project directory.
+- Run poetry install.
+- Run poetry shell.
+- Run the command: 
+    - python tracker.py
+- Check inbox for notification.
+
+## Preview
+#### ![env file](preview-env.png)
+#### ![email](preview-email.png)
---
+++ tracker.py
@@ -0,0 +1,39 @@
+import requests
+import lxml
+import os
+from bs4 import BeautifulSoup
+import smtplib
+from dotenv import load_dotenv
+
+load_dotenv()
+
+PRODUCT_URL = os.environ.get("PRODUCT_URL")
+ORIGIN_EMAIL = os.environ.get("ORIGIN_EMAIL")
+PASSW = os.environ.get("PASSWORD")
+RECEIVER = os.environ.get("RECEIVER_EMAIL")
+MINIMAL_PRICE = os.environ.get("PRICE")
+
+headers = {
+    "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36",
+    "accept-language": "es-ES,es;q=0.9,en;q=0.8",
+}
+response = requests.get(PRODUCT_URL, headers=headers)
+# print(response.status_code)
+
+soup = BeautifulSoup(response.content, "lxml")
+locate_price = soup.find(id="price_inside_buybox").get_text()
+price = locate_price.split("$")[1].strip()
+price_unformatted = price.split(",")
+price_formatted = price_unformatted[0] + price_unformatted[1]
+cost = float(price_formatted)
+
+email_message = None
+
+if cost < float(MINIMAL_PRICE):
+    email_message = f"Subject:Amazon Price Drop\n\nDude your product is at ${cost}.\nGO! GO! GO! {PRODUCT_URL}."
+    with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+        connection.starttls()
+        connection.login(user=ORIGIN_EMAIL, password=PASSW)
+        connection.sendmail(
+            from_addr=ORIGIN_EMAIL, to_addrs=RECEIVER, msg=email_message.encode("utf-8")
+        )
